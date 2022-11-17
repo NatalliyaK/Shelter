c643043e1a6f8c0eb34648b8596484da07175dd0
Natallya <natasupernice@gmail.com>
2022-11-17 16:40:48 +0300

    fixed webpackconfig file, add svgo, made help section

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = ({ development }) => ({
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
-        assetModuleFilename: 'assets/[hash][ext]',
+        assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
         clean: true,
     },
     plugins:
@@ -36,51 +36,29 @@ module.exports = ({ development }) => ({
                 ],
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                test: /\.(png|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
             },
             {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                type: "asset",
+                test: /\.svg$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: path.join('icons', '[name].[contenthash][ext]'),
+                },
             },
-        ]
+          ]
     },
     optimization: {
         minimizer: [
-            "...",
             new ImageMinimizerPlugin({
                 minimizer: {
                     implementation: ImageMinimizerPlugin.imageminMinify,
                     options: {
-                        // Lossless optimization with custom option
-                        // Feel free to experiment with options for better result for you
                         plugins: [
                             ["gifsicle", { interlaced: true }],
                             ["jpegtran", { progressive: true }],
                             ["optipng", { optimizationLevel: 5 }],
-                            // Svgo configuration here https://github.com/svg/svgo#configuration
-                            [
-                                "svgo",
-                                {
-                                    plugins: [
-                                        {
-                                            name: "preset-default",
-                                            params: {
-                                                overrides: {
-                                                    removeViewBox: false,
-                                                    addAttributesToSVGElement: {
-                                                        params: {
-                                                            attributes: [
-                                                                { xmlns: "http://www.w3.org/2000/svg" },
-                                                            ],
-                                                        },
-                                                    },
-                                                },
-                                            },
-                                        },
-                                    ],
-                                },
-                            ],
+                            ['svgo', { name: 'preset-default' }],
                         ],
                     },
                 },
